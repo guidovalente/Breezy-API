7e9ced55a12ae5c04bcd2db4d73d4439b97032e3
Guido Valente <guidovalente@outlook.com>
2019-11-06 22:54:20 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Python class for consuming Breezy's API.
+
+Breezy's API documentation can be found at https://https://developer.breezy.hr/
+
+
+## Requirements
+Requests and PyYAML is necessary to run this code:
+
+`pip install requests pyyaml`
+
+
+## Usage
+
+Before using the code, make sure to create an auth.yaml file and include your credentials:
+
+```yaml
+email: your@email.here
+password: breezyPassword
+```
+
+Then you can import the BreezyAPI class and make your requests:
+
+```python
+# import BreezyAPI from breezy.py
+from breezy import BreezyAPI
+
+# create a BreezyAPI object
+breezy = BreezyAPI()
+
+
+# Upon instantiaton, the breezy object will automatically call the /signi
+# endpoint and obtain a token, if it fails to do it will raise an exception.
+print(breezy.token)  # will print the generated token
+
+# you will now be able to communicate with the API
+
+# for example, we can get the companies' ids, which will be required for most
+# of Breezy's API calls
+companies = breezy.call(
+    "companies" 
+)
+
+```
---
+++ breezy.py
@@ -0,0 +1,56 @@
+import requests
+import yaml
+import sys
+
+class BreezyError(Exception):
+    pass
+
+
+class BreezyAPI:
+    API_URL = "https://api.breezy.hr/v3/"
+
+    def __init__(self):
+        with open('auth.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+        self.email = config['email']
+        self.password = config['password']
+        self.token = self._get_token()
+
+    def call(self, endpoint: str, method: str = 'GET', params: dict = None,
+             data: dict = None) -> dict:
+        assert method in ['GET', 'POST', 'PUT'], 'Wrong method provided'
+        while True:
+            response = requests.request(
+                method,
+                self.API_URL + endpoint,
+                headers={"Authorization": self.token},
+                params=params,
+                data=data
+            )
+            if response.status_code == 401:
+                print('Status code 401, getting new token')
+                self.token = self._get_token()
+            elif response.status_code == 429:
+                print('Exceeded API rate limit, sleeping 10 seconds')
+                sleep(10)
+            elif response.status_code == 200:
+                return response.json()
+            elif response.status_code == 500:
+                raise BreezyError(response.json()['error']['message'])
+            elif response.status_code == 504:
+                print('504 error, sleeping 30 seconds')
+                sleep(30)
+            else:
+                raise Exception(f'response code: {response.status_code}\n'
+                                f'response text: {response.text}')
+
+    def _get_token(self) -> str:
+        token = requests.post(
+            self.API_URL + "signin",
+            data={"email": self.email, "password": self.password}
+        )
+        if token.status_code != 200:
+            raise BreezyError('Error obtaining token\nError '
+                              f'{token.status_code} > '
+                              f'{token.json()["error"]["message"]}')
+        return token.json()['access_token']
